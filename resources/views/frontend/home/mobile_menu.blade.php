d1b762837b1d89e08c09db2f0f9a0bffe7d3664d
ahmedelkasmioui12 <146140346+ahmedelkasmioui12@users.noreply.github.com>
2024-08-15 15:23:35 +0100

    last commit message

--- resources/views/frontend/home/mobile_menu.blade.php
+++ resources/views/frontend/home/mobile_menu.blade.php
@@ -5,11 +5,14 @@
             <nav class="menu-box">
                 <div class="nav-logo"><a href="index.html"><img src="{{ asset('frontend/assets/images/logo.png') }}" alt="" title=""></a></div>
                 <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+                <div class="btn-box">
+                    <a href="{{ route('user.properties.create') }}"  class="theme-btn btn-one"><span>+</span>Add Listing</a>
+                </div>
                 <div class="contact-info">
                     <h4>Contact Info</h4>
                     <ul>
                         <li>Nador  / Manzeh / Oriental </li>
-                        <li><a href="tel:+212608058157">+212608058157</a></li>
+                        <li><a href="tel:+212637654206">+212608058157</a></li>
                         <li><a href="mailto:tadartino.ma@gmail.com">tadartino.ma@gmail.com</a></li>
                     </ul>
                 </div>
