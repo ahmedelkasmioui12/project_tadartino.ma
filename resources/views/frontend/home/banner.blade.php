de0a96abb665beee2a23f5a397608f21aed4f42e
ahmedelkasmioui12 <146140346+ahmedelkasmioui12@users.noreply.github.com>
2024-08-15 16:40:39 +0100

    last commit message

--- resources/views/frontend/home/banner.blade.php
+++ resources/views/frontend/home/banner.blade.php
@@ -2,8 +2,8 @@
 $states = App\Models\State::latest()->get();
 $ptypes = App\Models\PropertyType::latest()->get();
  @endphp
- <section class="banner-section" style="background-image: url({{ asset('frontend/assets/images/banner/banner-1.jpg') }});">
-            <div class="auto-container">
+<section class="banner-section" style="background-image: url({{ asset('frontend/assets/images/banner/banner-1.jpg') }}); background-size: cover; background-position: center; background-repeat: no-repeat;">
+    <div class="auto-container">
                 <div class="inner-container">
                     <div class="content-box centred">
                         <h2>Tadartino.ma is a real estate platform.
